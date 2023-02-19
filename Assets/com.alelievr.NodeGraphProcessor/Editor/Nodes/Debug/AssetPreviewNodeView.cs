28282b13461a97b1ad4c7d0ba6a5e59e16b41d46
li.yubin <liyubin724@163.com>
2023-02-20 00:11:58 +0800

    feat: 添加Unity对象的预览的结点

---
+++ Assets/com.alelievr.NodeGraphProcessor/Editor/Nodes/Debug/AssetPreviewNodeView.cs
@@ -0,0 +1,26 @@
+using GraphProcessor;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+[NodeCustomEditor(typeof(AssetPreviewNode))]
+public class AssetPreviewNodeView : BaseNodeView
+{
+    public override void Enable()
+    {
+        var previewNode = nodeTarget as AssetPreviewNode;
+
+        var preview = new Image();
+        previewNode.onProcessed += () =>
+        {
+            if(previewNode.input == null)
+            {
+                preview.image= null;
+            }
+            else
+            {
+                preview.image = AssetPreview.GetAssetPreview(previewNode.input) ?? AssetPreview.GetMiniThumbnail(previewNode.input);
+            }
+        };
+        controlsContainer.Add(preview);
+    }
+}
---
+++ Assets/com.alelievr.NodeGraphProcessor/Runtime/Nodes/Debug/AssetPreviewNode.cs
@@ -0,0 +1,10 @@
+﻿using GraphProcessor;
+using UnityObject = UnityEngine.Object;
+
+[System.Serializable, NodeMenuItem("Debug/Asset Preview")]
+[NodeIdentity("AssetPreview")]
+public class AssetPreviewNode : BaseNode
+{
+    [Input(name ="input")]
+    public UnityObject input;
+}
