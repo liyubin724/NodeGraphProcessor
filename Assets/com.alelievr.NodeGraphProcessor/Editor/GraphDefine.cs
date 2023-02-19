d33092b2fe816b7784f090743585089f644515ba
li.yubin <liyubin724@163.com>
2023-02-20 01:20:31 +0800

    feat: 为port根据类型添加内置的样式

---
+++ Assets/com.alelievr.NodeGraphProcessor/Editor/GraphDefine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace GraphProcessor
+{
+    public static class PortDefine
+    {
+        [CustomPortTypeStyle]
+        public static List<string> PortStyleSheets
+        {
+            get
+            {
+                return new List<string>() {
+                    "GraphProcessorStyles/PortTypeView"
+                };
+            }
+        }
+    }
+}
