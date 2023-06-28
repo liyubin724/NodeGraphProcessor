35aea3ffcfa52ef84aa0888c48c5110f35580daa
liyubin <liyubin@kingsoft.com>
2023-06-28 20:11:07 +0800

    feat: 为port添加悬浮时对应类型提示

--- Assets/com.alelievr.NodeGraphProcessor/Editor/Views/PortView.cs
+++ Assets/com.alelievr.NodeGraphProcessor/Editor/Views/PortView.cs
@@ -52,9 +52,9 @@ namespace GraphProcessor
 
 			if (portData.vertical)
 				AddToClassList("Vertical");
-			
-			this.tooltip = portData.tooltip;
-		}
+
+            this.tooltip = string.IsNullOrEmpty(portData.tooltip) ? portType.ToString() : portData.tooltip;
+        }
 
 		public static PortView CreatePortView(Direction direction, FieldInfo fieldInfo, PortData portData, BaseEdgeConnectorListener edgeConnectorListener)
 		{
@@ -113,8 +113,9 @@ namespace GraphProcessor
 			if (name != null)
 				portName = name;
 			visualClass = "Port_" + portType.Name;
-			tooltip = portData.tooltip;
-		}
+
+            tooltip = string.IsNullOrEmpty(portData.tooltip) ? portType.ToString() : portData.tooltip;
+        }
 
 		public override void Connect(Edge edge)
 		{
