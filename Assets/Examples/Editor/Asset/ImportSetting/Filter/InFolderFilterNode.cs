a0760f35b5df55a59f1590ebced1abc222ff76cb
li.yubin <liyubin724@163.com>
2023-07-08 01:32:56 +0800

    feat: 添加根据目录筛选资源的结点

--- Assets/Examples/Editor/Asset/ImportSetting/Filter/InFolderFilterNode.cs
+++ Assets/Examples/Editor/Asset/ImportSetting/Filter/InFolderFilterNode.cs
@@ -1,12 +1,18 @@
 ﻿using GraphProcessor;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace GameEditor.Asset.ImportSetting
 {
+    public enum FolderType
+    {
+        Parent = 0,
+        AnyOfParent,
+        StartsWith,
+        EndsWith,
+        Contains,
+    }
+
 
     [Serializable]
     [Node("In Folder Filter")]
@@ -14,9 +20,44 @@ namespace GameEditor.Asset.ImportSetting
     [NodeTag("asset-importer")]
     public class InFolderFilterNode : BaseFilterNode
     {
+        public string folderName;
+        public FolderType folderType = FolderType.Parent;
+
         protected override bool IsValid(string assetPath)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(assetPath)) return false;
+
+            string folderPath = Path.GetDirectoryName(assetPath).Replace("\\","/");
+            if (folderType == FolderType.AnyOfParent || folderType == FolderType.Parent)
+            {
+                string[] parentFolders = folderPath.Split('/');
+                if (parentFolders.Length == 0)
+                {
+                    return false;
+                }
+                if (folderType == FolderType.AnyOfParent)
+                {
+                    return Array.IndexOf(parentFolders, folderName) >= 0;
+                }
+                else
+                {
+                    return parentFolders[parentFolders.Length - 1] == folderName;
+                }
+            }
+            else if (folderType == FolderType.StartsWith)
+            {
+                return folderPath.StartsWith(folderName);
+            }
+            else if (folderType == FolderType.EndsWith)
+            {
+                return folderPath.EndsWith(folderName);
+            }
+            else if (folderType == FolderType.Contains)
+            {
+                return folderPath.Contains(folderName);
+            }
+
+            return false;
         }
     }
 }
