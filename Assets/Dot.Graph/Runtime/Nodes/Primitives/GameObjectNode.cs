ee5efb792b91733f45d92894a3f2ec5408240949
li.yubin <liyubin724@163.com>
2024-04-13 13:03:59 +0800

    feat: 添加新的GameObject结点

---
+++ Assets/Dot.Graph/Runtime/Nodes/Primitives/GameObjectNode.cs
@@ -0,0 +1,18 @@
+﻿using GraphProcessor;
+using System;
+using UnityEngine;
+
+namespace DotEngine.Graph
+{
+    [Serializable]
+    [NodeIdentity("GameObject", new string[] { "primitives", "common" })]
+    [NodeMenuItem("Common/Primitives/GameObject")]
+    public class GameObjectNode : PrimitiveNode<GameObject>, ICreateNodeFrom<GameObject>
+    {
+        public bool InitializeNodeFromObject(GameObject value)
+        {
+            this.value = value;
+            return true;
+        }
+    }
+}
