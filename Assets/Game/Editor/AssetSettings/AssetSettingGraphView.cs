4d3ae71ea5cb39e981a135c7dcf4763074c9941b
li.yubin <liyubin724@163.com>
2024-04-15 00:39:41 +0800

    feat: 添加基于Graph的Asset设置

---
+++ Assets/Game/Editor/AssetSettings/AssetSettingGraphView.cs
@@ -0,0 +1,29 @@
+﻿using GraphProcessor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace DotEditor.AssetSettings
+{
+    public class AssetSettingGraphView : BaseGraphView
+    {
+        public AssetSettingGraphView(EditorWindow window) : base(window)
+        {
+        }
+
+        public override bool FilterCreateNodeMenuEntry(string path, Type nodeType)
+        {
+            var identityAttr = nodeType.GetCustomAttribute<NodeIdentityAttribute>();
+            if (identityAttr == null || identityAttr.tags == null || identityAttr.tags.Length == 0)
+            {
+                return false;
+            }
+
+            return base.FilterCreateNodeMenuEntry(path, nodeType);
+        }
+    }
+}
