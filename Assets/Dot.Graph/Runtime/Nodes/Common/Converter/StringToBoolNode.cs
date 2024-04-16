62b465a3d24edf79cde0e3851fa5ec89d89b9480
li.yubin <liyubin724@163.com>
2024-04-17 00:08:17 +0800

    feat: 添加string到bool转换结点

---
+++ Assets/Dot.Graph/Runtime/Nodes/Common/Converter/StringToBoolNode.cs
@@ -0,0 +1,23 @@
+﻿using GraphProcessor;
+using System;
+
+namespace DotEngine.Graph
+{
+    [Serializable]
+    [NodeIdentity("String To Bool", new string[] { "converter", "common" })]
+    [NodeMenuItem("Common/Converter/String To Bool")]
+    public class StringToBoolNode : ConverterNode<string, bool>
+    {
+        protected override void OnConvert()
+        {
+            if (bool.TryParse(input, out bool result))
+            {
+                output = result;
+            }
+            else
+            {
+                output = false;
+            }
+        }
+    }
+}
