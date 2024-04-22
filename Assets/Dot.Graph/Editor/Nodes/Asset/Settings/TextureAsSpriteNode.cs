d315310c7363dcac697e9a2b101d2a77501cedd4
liyubin <liyubin@kingsoft.com>
2024-04-22 12:09:25 +0800

    opt: 优化结点可以接收多个连接

--- Assets/Dot.Graph/Editor/Nodes/Asset/Settings/TextureAsSpriteNode.cs
+++ Assets/Dot.Graph/Editor/Nodes/Asset/Settings/TextureAsSpriteNode.cs
@@ -1,5 +1,7 @@
 ﻿using GraphProcessor;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -31,6 +33,30 @@ namespace DotEditor.Graph.Assets
         public TextureImporterPlatformSettings androidSetting;
         [Input]
         public TextureImporterPlatformSettings iosSetting;
+
+        [CustomPortInput(nameof(assetPaths), typeof(string[]), allowCast = true)]
+        public void GetInputs(List<SerializableEdge> edges)
+        {
+            var allInputPaths = edges.Select(e => (string[])e.passThroughBuffer).ToArray();
+            foreach (var inputPaths in allInputPaths)
+            {
+                foreach (var path in inputPaths)
+                {
+                    if (string.IsNullOrEmpty(path)) continue;
+                    if (Array.IndexOf(assetPaths, path) >= 0) continue;
+
+                    var tempPaths = assetPaths;
+                    assetPaths = new string[tempPaths.Length + 1];
+                    Array.Copy(tempPaths, assetPaths, tempPaths.Length);
+                    assetPaths[assetPaths.Length - 1] = path;
+                }
+            }
+        }
+
+        protected override void BeforePullInputDatas()
+        {
+            assetPaths = new string[0];
+        }
 
         protected override void Process()
         {
