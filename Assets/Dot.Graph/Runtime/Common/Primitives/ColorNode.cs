af64b2c1ae45e23881db1197153da6a555d3543a
li.yubin <liyubin724@163.com>
2024-04-24 00:32:36 +0800

    feat: 添加新的Color结点

---
+++ Assets/Dot.Graph/Runtime/Common/Primitives/ColorNode.cs
@@ -0,0 +1,13 @@
+﻿using GraphProcessor;
+using System;
+using UnityEngine;
+
+namespace DotEngine.Graph
+{
+    [Serializable]
+    [NodeIdentity("Color", new string[] { "primitives", "common" })]
+    [NodeMenuItem("Common/Primitives/Color")]
+    public class ColorNode : PrimitiveNode<Color>
+    {
+    }
+}
